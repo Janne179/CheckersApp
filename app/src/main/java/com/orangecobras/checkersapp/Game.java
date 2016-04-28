78c49085d6e54070ee95d4a2c05f9fb18296130b
Hendrikto <hendrik.to@gmail.com>
2016-04-28 21:18:48 +0200

    Add an attribute to hold a grid view.

--- app/src/main/java/com/orangecobras/checkersapp/Game.java
+++ app/src/main/java/com/orangecobras/checkersapp/Game.java
@@ -3,13 +3,17 @@ package com.orangecobras.checkersapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 
 public class Game extends AppCompatActivity {
+
+    private GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        gridView = (GridView) findViewById(R.id.gridViewGame);
     }
 
     public void finish(View view) {
